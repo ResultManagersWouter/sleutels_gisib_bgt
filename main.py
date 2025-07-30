bba7ea05e5bb19210684001dd0fad71f569693ad
Wouter Schoenmakers <w.k.schoenmakers@gmail.com>
2025-07-30 19:58:24 +0200

    changed format

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 # This is a sample Python script.
 import os
 import logging
-from dataloaders import read_gisib,read_bgt,read_bgt_shapes,read_controle_tabel
+from dataloaders import read_gisib, read_bgt, read_bgt_shapes, read_controle_tabel
 from dotenv import load_dotenv
 from gisib_validator import GisibValidator
-from enums import AssetType, Gebied,ControleTabelGisib,ObjectType
-from matchers import GroenobjectenMatcher,TerreindelenMatcher,VerhardingenMatcher
-from columns_config import BGT_COLUMNS,ASSET_SCHEMAS
+from enums import AssetType, Gebied, ControleTabelGisib, ObjectType
+from matchers import GroenobjectenMatcher, TerreindelenMatcher, VerhardingenMatcher
+from columns_config import BGT_COLUMNS, ASSET_SCHEMAS
 from controller import Controller
 from datetime import date
+
 logger = logging.getLogger(__name__)
 
 load_dotenv()
 logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s | %(levelname)s | %(message)s'
+    level=logging.INFO, format="%(asctime)s | %(levelname)s | %(message)s"
 )
 
 gisib_id_col = "GUID"
@@ -24,30 +24,40 @@ gisib_objecttype_col = "OBJECTTYPE"
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+if __name__ == "__main__":
     # controle tabel
-    controle_tabel = read_controle_tabel(filepath=os.environ.get("FP_CONTROLE_TABEL"),
-                                         columns = ...,
-                                         filterEnum=ControleTabelGisib,
-                                         filter_col=ObjectType.CONTROLE_TABEL.value)
+    controle_tabel = read_controle_tabel(
+        filepath=os.environ.get("FP_CONTROLE_TABEL"),
+        columns=...,
+        filterEnum=ControleTabelGisib,
+        filter_col=ObjectType.CONTROLE_TABEL.value,
+    )
 
     # Load assets
     assets = {
-        AssetType.TERREINDEEL: read_gisib(fp_gisib=os.environ.get('FP_TRD'),
-                                          columns=ASSET_SCHEMAS[AssetType.TERREINDEEL],
-                                          layer=AssetType.TERREINDEEL),
-        AssetType.GROENOBJECTEN: read_gisib(fp_gisib=os.environ.get("FP_GRN"),
-                                            columns=ASSET_SCHEMAS[AssetType.GROENOBJECTEN],
-                                            layer=AssetType.GROENOBJECTEN),
-        AssetType.VERHARDINGEN: read_gisib(fp_gisib=os.environ.get("FP_VRH"),
-                                           columns=ASSET_SCHEMAS[AssetType.VERHARDINGEN],
-                                           layer=AssetType.VERHARDINGEN),
+        AssetType.TERREINDEEL: read_gisib(
+            fp_gisib=os.environ.get("FP_TRD"),
+            columns=ASSET_SCHEMAS[AssetType.TERREINDEEL],
+            layer=AssetType.TERREINDEEL,
+        ),
+        AssetType.GROENOBJECTEN: read_gisib(
+            fp_gisib=os.environ.get("FP_GRN"),
+            columns=ASSET_SCHEMAS[AssetType.GROENOBJECTEN],
+            layer=AssetType.GROENOBJECTEN,
+        ),
+        AssetType.VERHARDINGEN: read_gisib(
+            fp_gisib=os.environ.get("FP_VRH"),
+            columns=ASSET_SCHEMAS[AssetType.VERHARDINGEN],
+            layer=AssetType.VERHARDINGEN,
+        ),
     }
 
     # based on the controletabel
     objecttypes = ...
     # bgt = read_bgt(fp_bgt=os.environ.get('FP_BGT'),columns=BGT_COLUMNS)
-    bgt = read_bgt_shapes(os.environ.get("FP_BGT_FOLDER"),)
+    bgt = read_bgt_shapes(
+        os.environ.get("FP_BGT_FOLDER"),
+    )
     level = Gebied.BUURT.value
     area = "all"
     #
@@ -56,21 +66,24 @@ if __name__ == '__main__':
     #     for key, df in assets.items()
     # }
     # check if there is overlap in gisib
-    validator = GisibValidator(assets=assets,
-                               gisib_id_col=gisib_id_col,
-                               relatieve_hoogteligging_col=gisib_hoogteligging_col,
-                               objecttype_col=gisib_objecttype_col,
-                               gpkg_path=f"overlaps_{area.lower()}.gpkg")
+    validator = GisibValidator(
+        assets=assets,
+        gisib_id_col=gisib_id_col,
+        relatieve_hoogteligging_col=gisib_hoogteligging_col,
+        objecttype_col=gisib_objecttype_col,
+        gpkg_path=f"overlaps_{area.lower()}.gpkg",
+    )
     valid = validator.run_all_validations()
     # # if there is no overlap, continue
     if valid.empty:
         controller = Controller(
             assets=assets,
-            bgt =bgt,
-             gisib_id_col =gisib_id_col,
-             bgt_id_col=bgt_id_col,
-             gisib_hoogteligging_col=gisib_hoogteligging_col,
-             bgt_hoogteligging_col="hoogtelig")
+            bgt=bgt,
+            gisib_id_col=gisib_id_col,
+            bgt_id_col=bgt_id_col,
+            gisib_hoogteligging_col=gisib_hoogteligging_col,
+            bgt_hoogteligging_col="hoogtelig",
+        )
         buckets = controller.create_buckets()
     #     if buckets:
     #         controller.match_gisib_bgt_ids(suffix=area.lower(),directory=date.today().isoformat() +"_" +area.lower())
@@ -89,7 +102,6 @@ if __name__ == '__main__':
     # # bgt_gisib = groen.build_bgt_gisib_grouped(intersection_df=overlaps)
     # intersection_df = gisib.preprocess()
     # buckets = gisib.run()
-
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
